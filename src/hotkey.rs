109e3f05e0a6c0d0b837f7552b95e44a1f26cf9f
Gr3atWh173 <Gr3atWh173@users.noreply.github.com>
2020-05-10 15:58:10 +0530

    add hotkey config

--- src/hotkey.rs
+++ src/hotkey.rs
@@ -4,22 +4,43 @@ use std::ptr;
 use std::thread;
 
 use winapi::um::winuser::{
-    DispatchMessageW, GetMessageW, MessageBoxW, RegisterHotKey, TranslateMessage, MB_OK, MOD_ALT,
-    MOD_CONTROL, MOD_NOREPEAT, WM_HOTKEY,
+    DispatchMessageW, GetMessageW, MessageBoxW, RegisterHotKey, TranslateMessage,
+    GetKeyboardLayout, VkKeyScanExA, MB_OK, MOD_ALT, MOD_CONTROL, MOD_SHIFT, 
+    MOD_NOREPEAT, WM_HOTKEY,
 };
 
 use crate::Message;
 use crate::CHANNEL;
 
+use crate::config;
+
 pub fn spawn_hotkey_thread() {
+    let mut activators: Vec<String> = vec!();
+    let virtual_key_char: char;
+    
+    let mut hotkey: Vec<String> = config::load_config().hotkey
+                                .split("+")
+                                .map(|s| s.to_string())
+                                .collect();
+
+    if hotkey.len() != 3 {
+        panic!("Invalid hotkey combination.");
+    }
+    
+    virtual_key_char = hotkey.pop().unwrap()
+                        .chars().next().unwrap();
+    
+    activators.push(hotkey.pop().unwrap());
+    activators.push(hotkey.pop().unwrap());
+
     thread::spawn(move || unsafe {
         let sender = &CHANNEL.0.clone();
 
         let result = RegisterHotKey(
             ptr::null_mut(),
             0,
-            MOD_CONTROL as u32 | MOD_ALT as u32 | MOD_NOREPEAT as u32,
-            0x53, // S
+            compile_activators(&activators) | MOD_NOREPEAT as u32,
+            get_vkcode(virtual_key_char),
         );
 
         if result == 0 {
@@ -47,4 +68,24 @@ pub fn spawn_hotkey_thread() {
             }
         }
     });
+}
+
+fn compile_activators(activators: &Vec<String>) -> u32 {
+    let mut code: u32 = 0;
+    for key in activators.iter() {
+        match key.as_str() {
+            "ALT"   => code = code | MOD_ALT as u32,
+            "CTRL"  => code = code | MOD_CONTROL as u32,
+            "SHIFT" => code = code | MOD_SHIFT as u32,
+            &_      => panic!("Invalid hotkey combination."),
+        }
+    }
+    code
+}
+
+unsafe fn get_vkcode(key_char: char) -> u32 {
+    let keyboard_layout = GetKeyboardLayout(0);
+    let vk_code = VkKeyScanExA(key_char as i8, keyboard_layout).to_be_bytes();
+    
+    vk_code[1] as u32
 }
