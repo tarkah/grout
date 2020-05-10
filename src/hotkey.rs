2d859950219642e9348e85073ee5ae67425fc572
Gr3atWh173 <Gr3atWh173@users.noreply.github.com>
2020-05-10 22:40:01 +0530

    refactor and error handling

--- src/hotkey.rs
+++ src/hotkey.rs
@@ -11,27 +11,22 @@ use winapi::um::winuser::{
 
 use crate::Message;
 use crate::CHANNEL;
-
 use crate::config;
 
 pub fn spawn_hotkey_thread() {
-    let mut activators: Vec<String> = vec!();
-    let virtual_key_char: char;
-    
     let mut hotkey: Vec<String> = config::load_config().hotkey
                                 .split("+")
                                 .map(|s| s.trim().to_string())
                                 .collect();
 
-    if hotkey.len() != 3 {
-        panic!("Invalid hotkey combination.");
+    if !!!(2..5).contains(&hotkey.len()) {
+        unsafe {
+            report_and_exit("Invalid hotkey: Combination must be between 2 to 4 keys long.");
+        }
     }
-    
-    virtual_key_char = hotkey.pop().unwrap()
-                        .chars().next().unwrap();
-    
-    activators.push(hotkey.pop().unwrap());
-    activators.push(hotkey.pop().unwrap());
+
+    let virtual_key_char = hotkey.pop().unwrap()
+                            .chars().next().unwrap();
 
     thread::spawn(move || unsafe {
         let sender = &CHANNEL.0.clone();
@@ -39,12 +34,12 @@ pub fn spawn_hotkey_thread() {
         let result = RegisterHotKey(
             ptr::null_mut(),
             0,
-            compile_activators(&activators) | MOD_NOREPEAT as u32,
+            compile_modifiers(&hotkey) | MOD_NOREPEAT as u32,
             get_vkcode(virtual_key_char),
         );
 
         if result == 0 {
-            let error_msg = "Failed to assign hot key. Is program already running?";
+            /*let error_msg = "Failed to assign hot key. Is program already running?";
             let mut error_msg = error_msg.encode_utf16().collect::<Vec<_>>();
             error_msg.push(0);
 
@@ -53,9 +48,9 @@ pub fn spawn_hotkey_thread() {
                 error_msg.as_mut_ptr(),
                 ptr::null_mut(),
                 MB_OK,
-            );
+            );*/
 
-            process::exit(1);
+            report_and_exit("Failed to assign hot key. Is program already running?");
         }
 
         let mut msg = mem::zeroed();
@@ -70,14 +65,17 @@ pub fn spawn_hotkey_thread() {
     });
 }
 
-fn compile_activators(activators: &Vec<String>) -> u32 {
+fn compile_modifiers(activators: &Vec<String>) -> u32 {
     let mut code: u32 = 0;
     for key in activators.iter() {
         match key.as_str() {
             "ALT"   => code = code | MOD_ALT as u32,
             "CTRL"  => code = code | MOD_CONTROL as u32,
             "SHIFT" => code = code | MOD_SHIFT as u32,
-            &_      => panic!("Invalid hotkey combination."),
+
+            _ => unsafe {
+                report_and_exit("Invalid hotkey: Unidentified modifier in hotkey combination.")
+            },
         }
     }
     code
@@ -88,4 +86,18 @@ unsafe fn get_vkcode(key_char: char) -> u32 {
     let vk_code = VkKeyScanExA(key_char as i8, keyboard_layout).to_be_bytes();
     
     vk_code[1] as u32
+}
+
+unsafe fn report_and_exit(error_msg: &str) {
+    let mut error_msg = error_msg.encode_utf16().collect::<Vec<_>>();
+    error_msg.push(0);
+
+    MessageBoxW(
+        ptr::null_mut(),
+        error_msg.as_mut_ptr(),
+        ptr::null_mut(),
+        MB_OK,
+    );
+
+    process::exit(1);
 }
